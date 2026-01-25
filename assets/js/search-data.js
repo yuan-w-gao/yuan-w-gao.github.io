7ee4d344713b5b1917cefe6c08bfc9e703b1144b
yuan-w-gao <33330097+yuan-w-gao@users.noreply.github.com>
2026-01-25 17:13:15 +0000

    Deploying to gh-pages from @ yuan-w-gao/yuan-w-gao.github.io@8b1bed995fe173737346a57036a627ea7b5249b4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,35 +511,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/resume_yuan_gao.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("mailto:%79%67%33%38%36 %61%74 %63%6C %64%6F%74 %63%61%6D %64%6F%74 %61%63 %64%6F%74 %75%6B", "_blank");
         },
       },{
         id: 'social-custom_social',
